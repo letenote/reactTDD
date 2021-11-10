abef08abdeb9e46df6547020c2807aa90fae5e06
aditReza <lete.note3@gmail.com>
2021-11-10 22:39:40 +0700

    add first init app test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("first render",() => {
+  test('renders learn react link', () => {
+    render(<App />);
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+  test('renders some text',() => {
+    render(<App />);
+    const getText = screen.getByText(/Edit/i)
+    expect(getText).toBeInTheDocument();
+  })
+})
